5fb04f502a4d509e713c9b30861e41a73064d988
Acefinfo <fromaashutoshthapa@gmail.com>
2026-02-18 07:58:35 +0545

    Array data type explained

---
+++ 2_Data_Type/5_Array.kt
@@ -0,0 +1,12 @@
+// Array is the collection of elements of the same type or its subtype of datatype, which is stored in contiguous memory.
+
+fun array (){
+    val arrays = arrayOf(1, 2, 3, 4, 5)
+    for (i in arrays){
+        println(i)
+    }
+}
+
+fun main(args: Array<String>) {
+    array()
+}
