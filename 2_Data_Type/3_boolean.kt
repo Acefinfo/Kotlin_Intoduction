4a959c3cbf73ba2a49905de2a8a847ec378e4a7a
Acefinfo <fromaashutoshthapa@gmail.com>
2026-02-18 07:57:24 +0545

    Boolean data type explained

---
+++ 2_Data_Type/3_boolean.kt
@@ -0,0 +1,10 @@
+
+// Understanding boolean data type in Kotlin
+fun main(){
+
+    val isKotlenFun: Boolean = true
+    val isFishTasty: Boolean = false
+
+    println("Is Kotlin fun? $isKotlenFun")
+    println("Is fish tasty? $isFishTasty")
+}
